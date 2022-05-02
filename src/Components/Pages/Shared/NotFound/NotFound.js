1bc2470355d43383de4761258eb1ee8e56ec9191
Toufiq Hasan Kiron <toufiqhasankiron2@gmail.com>
2022-05-02 23:28:17 +0600

    added footer

--- src/Components/Pages/Shared/NotFound/NotFound.js
+++ src/Components/Pages/Shared/NotFound/NotFound.js
@@ -1,11 +1,13 @@
 import React from "react";
 import PageTitle from "../PageTitle/PageTitle";
+import notfound from '../../../Assets/logo/404.jpg';
+import './NotFound.css';
 
 const NotFound = () => {
   return (
-    <div>
+    <div className="not-found">
       <PageTitle title="404"></PageTitle>
-      <h2>NotFound</h2>
+      <img src={notfound} alt="" />
     </div>
   );
 };
